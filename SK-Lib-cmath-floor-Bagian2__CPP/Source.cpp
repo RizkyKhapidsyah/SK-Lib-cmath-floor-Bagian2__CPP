eb5ab4a0686500135c5f331cbf948d3c6cd3abec
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-07-17 14:45:28 +0700

    Add project files.

---
+++ SK-Lib-cmath-floor-Bagian2__CPP/Source.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include <cmath>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+	
+	cout << "Floor is : " << floor(2.3) << endl;
+	cout << "Floor is : " << floor(-2.3) << endl;
+
+	_getch();
+	return 0;
+}
